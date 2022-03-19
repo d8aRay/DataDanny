e32a1531664ee3b08cbc7c7074029801016f7d30
ray <redwards1230@gmail.com>
2022-03-18 20:51:42 -0400

    Make schema fit for BQ

--- Week2: Pizza Runner/schema.sql
+++ Week2: Pizza Runner/schema.sql
@@ -1,105 +1,116 @@
 CREATE SCHEMA pizza_runner;
-SET search_path = pizza_runner;
 
-DROP TABLE IF EXISTS runners;
-CREATE TABLE runners (
-  "runner_id" INTEGER,
-  "registration_date" DATE
-);
-INSERT INTO runners
-  ("runner_id", "registration_date")
+DROP TABLE IF EXISTS pizza_runner.runners;
+CREATE TABLE pizza_runner.runners (
+  runner_id INTEGER,
+  registration_date DATE
+)
+;
+INSERT INTO pizza_runner.runners
+  (runner_id, registration_date)
 VALUES
   (1, '2021-01-01'),
   (2, '2021-01-03'),
   (3, '2021-01-08'),
-  (4, '2021-01-15');
+  (4, '2021-01-15')
+ ;
 
+DROP TABLE IF EXISTS pizza_runner.customer_orders;
+CREATE TABLE pizza_runner.customer_orders (
+  order_id INTEGER,
+  customer_id INTEGER,
+  pizza_id INTEGER,
+  exclusions STRING(4), --maybe better stored as an array in bq
+  extras STRING(4), --maybe better stored as an array in bq
+  order_time TIMESTAMP
+)
+;
 
-DROP TABLE IF EXISTS customer_orders;
-CREATE TABLE customer_orders (
-  "order_id" INTEGER,
-  "customer_id" INTEGER,
-  "pizza_id" INTEGER,
-  "exclusions" VARCHAR(4),
-  "extras" VARCHAR(4),
-  "order_time" TIMESTAMP
-);
-
-INSERT INTO customer_orders
-  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+INSERT INTO pizza_runner.customer_orders
+  (order_id, customer_id, pizza_id, exclusions, extras, order_time)
 VALUES
-  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
-  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
-  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
-  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
-  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
-  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
-  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
-  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
-  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
-  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
-  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
-  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
-  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
-  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+  (1, 101, 1, NULL, NULL, '2020-01-01 18:05:02'),
+  (2, 101, 1, NULL, NULL, '2020-01-01 19:00:52'),
+  (3, 102, 1, NULL, NULL, '2020-01-02 23:51:23'),
+  (3, 102, 2, NULL, NULL, '2020-01-02 23:51:23'),
+  (4, 103, 1, '4', NULL, '2020-01-04 13:23:46'),
+  (4, 103, 1, '4', NULL, '2020-01-04 13:23:46'),
+  (4, 103, 2, '4', NULL, '2020-01-04 13:23:46'),
+  (5, 104, 1, NULL, '1', '2020-01-08 21:00:29'),
+  (6, 101, 2, NULL, NULL, '2020-01-08 21:03:13'),
+  (7, 105, 2, NULL, '1', '2020-01-08 21:20:29'),
+  (8, 102, 1, NULL, NULL, '2020-01-09 23:54:33'),
+  (9, 103, 1, '4', '1, 5', '2020-01-10 11:22:59'),
+  (10, 104, 1, NULL, NULL, '2020-01-11 18:34:49'),
+  (10, 104, 1, '2, 6', '1, 4', '2020-01-11 18:34:49')
+;
 
 
-DROP TABLE IF EXISTS runner_orders;
-CREATE TABLE runner_orders (
-  "order_id" INTEGER,
-  "runner_id" INTEGER,
-  "pickup_time" VARCHAR(19),
-  "distance" VARCHAR(7),
-  "duration" VARCHAR(10),
-  "cancellation" VARCHAR(23)
-);
 
-INSERT INTO runner_orders
-  ("order_id", "runner_id", "pickup_time", "distance", "duration", "cancellation")
+
+DROP TABLE IF EXISTS pizza_runner.runner_orders;
+CREATE TABLE pizza_runner.runner_orders (
+  order_id INTEGER,
+  runner_id INTEGER,
+  pickup_time TIMESTAMP,
+  distance STRING(7), --this should be an integer called distance_km
+  duration STRING(10), --this should be an integer called duration_sec 
+  cancellation STRING(23)
+)
+;
+
+INSERT INTO pizza_runner.runner_orders
+  (order_id, runner_id, pickup_time, distance, duration, cancellation)
 VALUES
-  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
-  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
-  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
-  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
-  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
-  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
-  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
-  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
-  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
-  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+  (1, 1, '2020-01-01 18:15:34', '20km', '32 minutes', NULL),
+  (2, 1, '2020-01-01 19:10:54', '20km', '27 minutes', NULL),
+  (3, 1, '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+  (4, 2, '2020-01-04 13:53:03', '23.4', '40', NULL),
+  (5, 3, '2020-01-08 21:10:57', '10', '15', NULL),
+  (6, 3, NULL, NULL, NULL, 'Restaurant Cancellation'),
+  (7, 2, '2020-01-08 21:30:45', '25km', '25mins', NULL),
+  (8, 2, '2020-01-10 00:15:02', '23.4 km', '15 minute', NULL),
+  (9, 2, NULL, NULL, NULL, 'Customer Cancellation'),
+  (10, 1, '2020-01-11 18:50:20', '10km', '10minutes', NULL)
+ ;
 
-
-DROP TABLE IF EXISTS pizza_names;
-CREATE TABLE pizza_names (
-  "pizza_id" INTEGER,
-  "pizza_name" TEXT
-);
-INSERT INTO pizza_names
-  ("pizza_id", "pizza_name")
+DROP TABLE IF EXISTS pizza_runner.pizza_names;
+CREATE TABLE pizza_runner.pizza_names (
+  pizza_id INTEGER,
+  pizza_name STRING(12)
+)
+;
+INSERT INTO pizza_runner.pizza_names
+  (pizza_id, pizza_name)
 VALUES
   (1, 'Meatlovers'),
-  (2, 'Vegetarian');
+  (2, 'Vegetarian')
+;
 
 
-DROP TABLE IF EXISTS pizza_recipes;
-CREATE TABLE pizza_recipes (
-  "pizza_id" INTEGER,
-  "toppings" TEXT
-);
-INSERT INTO pizza_recipes
-  ("pizza_id", "toppings")
+DROP TABLE IF EXISTS pizza_runner.pizza_recipes;
+CREATE TABLE pizza_runner.pizza_recipes (
+  pizza_id INTEGER,
+  toppings STRING(50)
+)
+;
+INSERT INTO pizza_runner.pizza_recipes
+  (pizza_id, toppings)
 VALUES
   (1, '1, 2, 3, 4, 5, 6, 8, 10'),
-  (2, '4, 6, 7, 9, 11, 12');
+  (2, '4, 6, 7, 9, 11, 12')
+  ;
 
 
-DROP TABLE IF EXISTS pizza_toppings;
-CREATE TABLE pizza_toppings (
-  "topping_id" INTEGER,
-  "topping_name" TEXT
-);
-INSERT INTO pizza_toppings
-  ("topping_id", "topping_name")
+DROP TABLE IF EXISTS pizza_runner.pizza_toppings;
+CREATE TABLE pizza_runner.pizza_toppings (
+  topping_id INTEGER,
+  topping_name STRING(15)
+)
+;
+
+INSERT INTO pizza_runner.pizza_toppings
+  (topping_id, topping_name)
 VALUES
   (1, 'Bacon'),
   (2, 'BBQ Sauce'),
@@ -112,4 +123,5 @@ VALUES
   (9, 'Peppers'),
   (10, 'Salami'),
   (11, 'Tomatoes'),
-  (12, 'Tomato Sauce');
+  (12, 'Tomato Sauce')
+  ;
